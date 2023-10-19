b0989436503024d769fc3795dbc97840e1335faf
finnbassill <tun39698@temple.edu>
2023-10-19 11:46:36 -0400

    Implemented newInstance of fragment & added fragment view in main xml.

--- app/src/main/java/edu/temple/inclassactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassactivity/MainActivity.kt
@@ -2,6 +2,7 @@ package edu.temple.inclassactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +15,10 @@ class MainActivity : AppCompatActivity() {
         typedArray.recycle()
 
         // Attach an instance of ImageDisplayFragment using factory method
-
+        val imageDisplay = ImageDisplayFragment.newInstance(imageArray)
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragmentContainerView, imageDisplay)
+            .commit()
     }
 }
